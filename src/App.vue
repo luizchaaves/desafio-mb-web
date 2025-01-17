fa6a0bfb564890a84ba2793d56bde721018bcc43
Luiz Otavio <locchavescarvalho@gmail.com>
2025-01-16 23:07:05 -0300

    feat: create step component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import Title from "./components/title.vue"
+import Steps from "./components/steps.vue"
 </script>
 <template>
   <div style="display: flex; flex-direction: column">
+    <Steps :step="2" />
     <Title title="Seja bem vindo(a)" />
   </div>
 </template>
---
+++ src/components/steps.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  step: {
+    type: Number,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <p>
+    Etapa <span>{{ step }}</span> de 4
+  </p>
+</template>
+
+<style lang="scss" scoped>
+p {
+  margin: 0px;
+  font-weight: $font-weight-semibold;
+
+  span {
+    color: $color-orange;
+  }
+}
+</style>
