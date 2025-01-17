81cb096706c99515c639864a0da92f769d10805a
Luiz Otavio <locchavescarvalho@gmail.com>
2025-01-17 12:42:38 -0300

    feat: create button component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
 import Title from "./components/title.vue"
 import Steps from "./components/steps.vue"
+import Button from "./components/button.vue"
 </script>
+
 <template>
   <div style="display: flex; flex-direction: column">
     <Steps :step="2" />
     <Title title="Seja bem vindo(a)" />
+    <Button label="Voltar" color="secondary" />
+    <Button label="Continuar" disabled @click="() => console.log('teste')" />
   </div>
 </template>
---
+++ src/components/button.vue
@@ -0,0 +1,80 @@
+<script setup>
+import { defineEmits, computed } from "vue"
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    default: "primary",
+    validator(value) {
+      return ["primary", "secondary"].includes(value)
+    },
+  },
+  stretched: Boolean,
+  disabled: Boolean,
+})
+
+const emit = defineEmits(["click"])
+
+const buttonClasses = computed(() => ({
+  primary: props.color === "primary",
+  secondary: props.color === "secondary",
+  disabled: props.disabled,
+}))
+
+const buttonStyles = computed(() => ({
+  width: props.stretched ? "100%" : "auto",
+}))
+
+const handleClick = () => {
+  if (!props.disabled) {
+    emit("click")
+  }
+}
+</script>
+
+<template>
+  <button :class="buttonClasses" :style="buttonStyles" @click="handleClick">
+    {{ label }}
+  </button>
+</template>
+
+<style lang="scss" scoped>
+button {
+  padding: $spacing-sm $spacing-md;
+  border-radius: $border-radius-md;
+  font-size: $font-size-sm;
+  cursor: pointer;
+  transition: background-color 0.25s;
+  width: 100%;
+  overflow: hidden;
+
+  &.primary {
+    border: 1px solid transparent;
+    background-color: $color-orange;
+    color: $color-white;
+
+    &:not(.disabled):hover {
+      background-color: $color-orange-hover;
+    }
+  }
+
+  &.secondary {
+    border: 1px solid $color-orange;
+    background-color: $color-white;
+    color: $color-orange;
+
+    &:not(.disabled):hover {
+      background-color: rgba($color: #000000, $alpha: 0.04);
+    }
+  }
+
+  &.disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+}
+</style>
