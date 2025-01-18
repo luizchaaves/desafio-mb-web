56bf75a52122063733e87b4d4dcf6bd701d5b00b
Luiz Otavio <locchavescarvalho@gmail.com>
2025-01-18 11:37:25 -0300

    feat: create the header component

---
+++ src/components/header.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Steps from "./steps.vue"
+import Title from "./title.vue"
+
+defineProps({
+  step: Number,
+  totalSteps: Number,
+  title: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <header class="register-header-container">
+    <Steps v-if="step && totalSteps" :step="step" :totalSteps="totalSteps" />
+    <Title :title="title" />
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.register-header-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: $spacing-xs;
+}
+</style>
