c2058631c564f972fe0b87d12b51185f8066528f
Luiz Otavio <locchavescarvalho@gmail.com>
2025-01-18 11:37:32 -0300

    feat: create the form component

---
+++ src/components/form.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { defineEmits, useSlots } from "vue"
+
+const emit = defineEmits(["submit"])
+const slots = useSlots()
+
+const onSubmit = () => {
+  emit("submit")
+}
+</script>
+
+<template>
+  <form @submit.prevent="onSubmit">
+    <div class="form-content">
+      <slot />
+    </div>
+    <div v-if="slots.buttons" class="form-buttons">
+      <slot name="buttons" />
+    </div>
+  </form>
+</template>
+
+<style lang="scss" scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  gap: $spacing-lg;
+
+  & > .form-content {
+    display: flex;
+    flex-direction: column;
+    gap: $spacing-md;
+  }
+
+  & > .form-buttons {
+    display: flex;
+    align-items: center;
+
+    gap: $spacing-lg;
+  }
+}
+</style>
