f330a8a5ede4f2b31c9168830c75cad5d612d308
Luiz Otavio <locchavescarvalho@gmail.com>
2025-01-19 03:00:54 -0300

    refactor: allow editing and validating data on the review page

--- src/components/registration-steps/review.vue
+++ src/components/registration-steps/review.vue
@@ -1,5 +1,18 @@
 <script setup>
-import { computed } from "vue"
+import { computed, reactive } from "vue"
+import {
+  formatDate,
+  formatPhone,
+  formatCpf,
+  formatCnpj,
+} from "../../utils/masks"
+import {
+  validateEmail,
+  validateCpf,
+  validateCnpj,
+  validateDate,
+  validatePhone,
+} from "../../utils/validators"
 import Form from "../form.vue"
 import Input from "../input.vue"
 import Button from "../button.vue"
@@ -17,61 +30,207 @@ const isPhysicsPerson = computed(
   () => props.registrationData.personType === "pp"
 )
 
-const handleRegistration = () => {}
+const formIsValid = computed(() => {
+  const validateCommonData =
+    Boolean(
+      props.registrationData.email &&
+        props.registrationData.phone &&
+        props.registrationData.password
+    ) &&
+    isValid.email &&
+    isValid.phone
 
-const handleBack = () => {
-  emit("prevStep")
+  if (isPhysicsPerson.value) {
+    return (
+      validateCommonData &&
+      Boolean(
+        props.registrationData.name &&
+          props.registrationData.cpf &&
+          props.registrationData.birthdate
+      ) &&
+      isValid.document &&
+      isValid.date
+    )
+  } else {
+    return (
+      validateCommonData &&
+      Boolean(
+        props.registrationData.company &&
+          props.registrationData.cnpj &&
+          props.registrationData.openingDate
+      ) &&
+      isValid.document &&
+      isValid.date
+    )
+  }
+})
+
+const isValid = reactive({
+  email: true,
+  document: true,
+  date: true,
+  phone: true,
+})
+
+const onChangeValidateEmail = () => {
+  isValid.email = validateEmail(props.registrationData.email)
+}
+
+const onChangeValidateCpf = () => {
+  isValid.document = validateCpf(props.registrationData.cpf)
+}
+
+const onChangeValidateCnpj = () => {
+  isValid.document = validateCnpj(props.registrationData.cnpj)
+}
+
+const onChangeValidateBirthdate = () => {
+  isValid.date = validateDate(props.registrationData.birthdate)
+}
+
+const onChangeValidateOpeningDate = () => {
+  isValid.date = validateDate(props.registrationData.openingDate)
+}
+
+const onChangeValidatePhone = () => {
+  isValid.phone = validatePhone(props.registrationData.phone)
+}
+
+const handleCpf = () => {
+  props.registrationData.cpf = formatCpf(props.registrationData.cpf)
+}
+
+const handleCnpj = () => {
+  props.registrationData.cnpj = formatCnpj(props.registrationData.cnpj)
+}
+
+const handlePhone = () => {
+  props.registrationData.phone = formatPhone(props.registrationData.phone)
+}
+
+const handleBirthdate = () => {
+  props.registrationData.birthdate = formatDate(
+    props.registrationData.birthdate
+  )
+}
+
+const handleOpeningDate = () => {
+  props.registrationData.openingDate = formatDate(
+    props.registrationData.openingDate
+  )
+}
+
+const handleRegistration = () => {
+  if (formIsValid.value) {
+  }
 }
 </script>
 
 <template>
-  <Form>
+  <Form @submit="handleRegistration">
     <Input
-      disabled
       label="Endereço de e-mail"
+      type="email"
+      inputmode="email"
+      :error="
+        !isValid.email && props.registrationData.email ? 'e-mail inválido' : ''
+      "
       v-model="props.registrationData.email"
+      @change="onChangeValidateEmail"
     />
 
     <Input
       v-if="isPhysicsPerson"
-      disabled
       label="Nome"
+      type="text"
       v-model="props.registrationData.name"
     />
+
     <Input
       v-else
-      disabled
       label="Razão social"
+      type="text"
       v-model="props.registrationData.company"
     />
 
     <Input
       v-if="isPhysicsPerson"
-      disabled
       label="CPF"
+      type="text"
+      :maxlength="14"
+      inputmode="numeric"
+      :error="
+        !isValid.document && props.registrationData.cpf ? 'cpf inválido' : ''
+      "
       v-model="props.registrationData.cpf"
+      @keyup="handleCpf"
+      @change="onChangeValidateCpf"
     />
-    <Input v-else disabled label="CNPJ" v-model="props.registrationData.cnpj" />
+
+    <Input
+      v-else
+      label="CNPJ"
+      type="text"
+      :maxlength="18"
+      inputmode="numeric"
+      :error="
+        !isValid.document && props.registrationData.cnpj ? 'cnpj inválido' : ''
+      "
+      v-model="props.registrationData.cnpj"
+      @keyup="handleCnpj"
+      @change="onChangeValidateCnpj"
+    />
 
     <Input
       v-if="isPhysicsPerson"
-      disabled
       label="Data de nascimento"
+      type="text"
+      :maxlength="10"
+      inputmode="numeric"
+      :error="
+        !isValid.date && props.registrationData.birthdate
+          ? 'data de nascimento inválida'
+          : ''
+      "
       v-model="props.registrationData.birthdate"
-    />
-    <Input
-      v-else
-      disabled
-      label="Data de abertura"
-      v-model="props.registrationData.openingDate"
+      @keyup="handleBirthdate"
+      @change="onChangeValidateBirthdate"
     />
 
-    <Input label="Telefone" disabled v-model="props.registrationData.phone" />
+    <Input
+      v-else
+      label="Data de abertura"
+      type="text"
+      :maxlength="10"
+      inputmode="numeric"
+      :error="
+        !isValid.date && props.registrationData.openingDate
+          ? 'data de abertura inválida'
+          : ''
+      "
+      v-model="props.registrationData.openingDate"
+      @keyup="handleOpeningDate"
+      @change="onChangeValidateOpeningDate"
+    />
+
+    <Input
+      label="Telefone"
+      type="tel"
+      :maxlength="15"
+      inputmode="tel"
+      :error="
+        !isValid.phone && props.registrationData.phone
+          ? 'telefone inválido'
+          : ''
+      "
+      v-model="props.registrationData.phone"
+      @keyup="handlePhone"
+      @change="onChangeValidatePhone"
+    />
 
     <Input
       label="Senha"
-      disabled
-      type="password"
+      type="text"
       v-model="props.registrationData.password"
     />
 
@@ -81,10 +240,14 @@ const handleBack = () => {
         stretched
         label="Voltar"
         color="secondary"
-        :disabled="false"
-        @click="handleBack"
+        @click="() => emit('prevStep')"
       />
-      <Button type="submit" stretched label="Continuar" :disabled="false" />
+      <Button
+        type="submit"
+        stretched
+        label="Continuar"
+        :disabled="!formIsValid"
+      />
     </template>
   </Form>
 </template>
